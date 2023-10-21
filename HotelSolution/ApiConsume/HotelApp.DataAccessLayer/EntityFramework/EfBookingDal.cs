4cf1d5044bb3339c8b354ea20ae7a23d14190f38
salihcancakar <salihxcan@hotmail.com>
2023-10-21 23:47:16 +0300

    Booking Part 2

--- HotelSolution/ApiConsume/HotelApp.DataAccessLayer/EntityFramework/EfBookingDal.cs
+++ HotelSolution/ApiConsume/HotelApp.DataAccessLayer/EntityFramework/EfBookingDal.cs
@@ -1,10 +1,11 @@
 namespace HotelApp.DataAccessLayer.EntityFramework;
 
+using Abstract;
 using Concrete;
 using EntityLayer.Concrete;
 using Repositories;
 
-public class EfBookingDal : GenericRepository<Booking>
+public class EfBookingDal : GenericRepository<Booking>,IBookingDal
 {
     public EfBookingDal(Context context) : base(context)
     {
